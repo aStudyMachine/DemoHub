a0dd1d1632ae783d902862bb2028f268c7584cbe
Marcus <studymachine@foxmail.com>
2025-04-11 23:30:15 +0800

    feat(rabbitmq): 添加消息发布确认和返回机制
    - 在 application.yaml 中添加 publisher-confirm-type 和 publisher-returns 配置
    - 在 RabbitmqConfig 中配置 RabbitTemplate 的确认回调和消息返回回调
    - 新增代码用于处理消息发布成功或失败的情况
    - 添加对未被路由到队列的消息的处理逻辑

--- rabbitmq-spring-demo/src/main/java/cn/studymachine/config/RabbitmqConfig.java
+++ rabbitmq-spring-demo/src/main/java/cn/studymachine/config/RabbitmqConfig.java
@@ -2,7 +2,10 @@ package cn.studymachine.config;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
@@ -22,8 +25,39 @@ public class RabbitmqConfig {
     /*---------------------------------------------- Methods ~ ----------------------------------------------*/
 
 
+    /**
+     * rabbit template 设置 发布确认回调 && 消息返回回调
+     */
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        // 发布确认回调
+        rabbitTemplate.setConfirmCallback((correlationData, ack, cause) -> {
+            if (ack) {
+                log.info("消息发送成功: {}", correlationData);
+            } else {
+                log.error("消息发送失败: {}, 原因: {}", correlationData, cause);
+            }
+        });
 
+        // // 旧版消息返回回调
+        // rabbitTemplate.setReturnCallback((message, replyCode, replyText, exchange, routingKey) -> {
+        //      log.error("消息未被路由到队列: {}, 回复码: {}, 回复文本: {}, 交换机: {}, 路由键: {}",
+        //             message, replyCode, replyText, exchange, routingKey);
+        // });
 
+        // 设置ReturnCallback（旧版用setReturnCallback，新版使用setReturnsCallback）
+        rabbitTemplate.setReturnsCallback(returned -> {
+            System.out.println("消息被退回：" +
+                    "\n消息主体：" + new String(returned.getMessage().getBody()) +
+                    "\n应答码：" + returned.getReplyCode() +
+                    "\n描述：" + returned.getReplyText() +
+                    "\n交换机：" + returned.getExchange() +
+                    "\n路由键：" + returned.getRoutingKey());
+        });
+
+        return rabbitTemplate;
+    }
 
 
 }
